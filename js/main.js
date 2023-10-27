c8c1f3f55959ce29da091a1e82a3adb51c024a9b
Juliana Quiñones <juli.bambo@gmail.com>
2023-10-27 17:14:30 -0300

    backsticks

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
 function agregarTotal(TotalProductos, PrecioProductos) {
-    return TotalProductos + PrecioProductos;
-  }
-  
-  function mostrarTotal(Nombreproducto, TotalProductos) {
-    console.log("Total ${Nombreproducto}: $${TotalProductos}");
-  }
-  
-  function agregarProducto(Nombreproducto) {
-    console.log("Producto agregado: ${Nombreproducto}");
-  }
-  
-  function main() {
-    let total = 0;
-    let i = 0;
-  
-    while (true) {
-      const Nombreproducto = "Producto ${i + 1}";
-      const PrecioProductos = parseFloat(prompt("Introduce el precio para ${Nombreproducto} (o presiona Cancelar para terminar):"));
-  
-      if (PrecioProductos !== "cancelar") {
-        
-        break;
+  return TotalProductos + PrecioProductos;
+}
+
+function mostrarTotal(Nombreproducto, TotalProductos) {
+  console.log(`Total ${Nombreproducto}: $${TotalProductos}`);
+}
+
+function agregarProducto(Nombreproducto) {
+  console.log(`Producto agregado: ${Nombreproducto}`);
+}
+
+function main() {
+  let total = 0;
+  let i = 0;
+
+  while (true) {
+      const Nombreproducto = `Producto ${i + 1}`;
+      const PrecioProductos = parseFloat(prompt(`Introduce el precio para ${Nombreproducto} (o presiona Cancelar para terminar):`));
+
+      if (PrecioProductos === null) {
+          break;
       }
-  
+
       total = agregarTotal(total, PrecioProductos);
       mostrarTotal(Nombreproducto, PrecioProductos);
       agregarProducto(Nombreproducto);
-  
+
       i++;
-    }
-  
-    console.log("Total de todos los productos: $${total}");
   }
-  
-  main();
-  
+
+  console.log(`Total de todos los productos: $${total}`);
+}
+
+main();
